d3c9f50d1bf42ce619193468fdaf4ccfcc51036b
Sulimovskyy Vladyslav <sulimovskyy.vladyslav@gmail.com>
2017-10-30 20:16:04 +0100

    FK angularjs obsolete junk

--- client/javascript/productPage.controller.js
+++ client/javascript/productPage.controller.js
@@ -1,12 +1,12 @@
 angular.module('mainApp')
     .controller('productPageController', ['$scope', 'Main', '$window', '$http', 'itemsWatchingSrv', function ($scope, Main, $window, $http, itemsWatchingSrv) {
+        //Fucking angularjs adding <option value="? undefined:undefined"> DONT TOUCH
         $scope.current = 0;
+        $scope.chosen = 1;
         var size = 0;
         Main.getItems({ 'id': $window.location.href.split("/product/")[1] }, (responseData) => {
             $scope.item = responseData;
             size = responseData.img.length;
-            // console.log(responseData.img);
-            // console.log(responseData.img.length);
         });
 
         $scope.next = () => {
@@ -14,7 +14,6 @@ angular.module('mainApp')
                 $scope.current = 0;
             }
         };
-
         $scope.addToWishist = () => {
             $http.post("http://localhost:8080/user/addToWishist", { "id": $window.location.href.split("/product/")[1] })
                 .then(function successCallback(response) {
