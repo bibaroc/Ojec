9670d7754a20dde249fd0ca3b4c3fa9f7dd06d0a
Sulimovskyy Vladyslav <sulimovskyy.vladyslav@gmail.com>
2017-11-13 16:41:49 +0100

    Small fixes, little typos

--- client/javascript/frontpage.controller.js
+++ client/javascript/frontpage.controller.js
@@ -1,16 +1,17 @@
 angular.module('mainApp')
-    .controller('frontPageController', ['$scope', '$location', 'Main', '$http','itemsWatchingSrv', 'cartSrv', function ($scope, $location, Main, $http, itemsWatchingSrv, cartSrv) {
+    .controller('frontPageController', ['$scope', '$location', 'Main', '$http','cartSrv', 'cartSrv', function ($scope, $location, Main, $http, cartSrv, cartSrv) {
         Main.getItems(null, (data) => {
             $scope.items = data;
         });
         $scope.rerout = (item) => {
             $location.path("/product/" + item._id);
         }
-        $scope.addToCart = (id) => {
-            $http.post("http://localhost:8080/user/addToCart", { "id": id, "qnt": $scope.chosen })
+        $scope.addToCart = (item) => {
+            $http.post("http://localhost:8080/user/addToCart", { "id": item._id, "qnt": $scope.chosen })
                 .then(function successCallback(response) {
                     if (response.data.success)
-                        itemsWatchingSrv.addProduct($scope.item);
+                    cartSrv.remove(item);
+                    cartSrv.addProduct(item, response.data.n);
                     $scope.cartMsg = response.data.msg;
                 }, function errorCallback(response) {
                     // alert("Something went wrong.")
