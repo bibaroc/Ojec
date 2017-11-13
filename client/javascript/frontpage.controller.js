04d62ebfbcf7e6e6d36c9f329c895ce786dbf333
luciabenigni <lucia.benigni@studenti.unicam.it>
2017-11-13 16:22:06 +0100

    frontpage

--- client/javascript/frontpage.controller.js
+++ client/javascript/frontpage.controller.js
@@ -1,9 +1,20 @@
 angular.module('mainApp')
-    .controller('frontPageController', ['$scope', '$location', 'Main', function ($scope, $location, Main) {
+    .controller('frontPageController', ['$scope', '$location', 'Main', '$http','itemsWatchingSrv', 'cartSrv', function ($scope, $location, Main, $http, itemsWatchingSrv, cartSrv) {
         Main.getItems(null, (data) => {
             $scope.items = data;
         });
         $scope.rerout = (item) => {
             $location.path("/product/" + item._id);
         }
+        $scope.addToCart = (id) => {
+            $http.post("http://localhost:8080/user/addToCart", { "id": id, "qnt": $scope.chosen })
+                .then(function successCallback(response) {
+                    if (response.data.success)
+                        itemsWatchingSrv.addProduct($scope.item);
+                    $scope.cartMsg = response.data.msg;
+                }, function errorCallback(response) {
+                    // alert("Something went wrong.")
+                });
+
+        };
     }]);
